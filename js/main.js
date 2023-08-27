4836ba0ec4241bfdc58ebc56d866b0a20936cf38
Heba Mansour <hebam1017@gmail.com>
2023-08-27 22:21:17 +0300

    up

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var productNameInput= document.getElementById('productNameInput');
 var productPriceInput= document.getElementById('productPriceInput');
 var productCategoryInput= document.getElementById('productCategoryInput');
 var productDescriptionInput= document.getElementById('productDescriptionInput');
+var textValidateName=document.getElementById('textValidateName')
+var textValidatePrice=document.getElementById('textValidatePrice')
+var textValidateCategory=document.getElementById('textValidateCategory')
+var textValidateDesc=document.getElementById('textValidateDesc')
 
 console.log(productNameInput,productPriceInput,productCategoryInput,productDescriptionInput);
 var  productContainer;
@@ -103,19 +107,83 @@ function updateProdut(){
       localStorage.setItem('my product',JSON.stringify(productContainer));
       displayProuduct(productContainer)
       clearForm()
+      addBtn.classList.replace('d-none','d-inline-block');
+      updateBtn.classList.add('d-none');
 }
 
+// function validateProductName(){
+// var regex=/^[A-Z][a-z]{3,8}$/;
+// if (regex.test(productNameInput.value) == true){
+//     productNameInput.classList.replace('is-invalid','is-valid')
+//  return true
+// }
+// else{
+//     productNameInput.classList.add('is-invalid')
+//     return false
+// }
+// ;
+
+
+// }
+
+
+
+//  ---------validation ---------------
 function validateProductName(){
-var regex=/^[A-Z][a-z]{3,8}$/;
-if (regex.test(productNameInput.value) == true){
-    productNameInput.classList.replace('is-invalid','is-valid')
- return true
-}
-else{
-    productNameInput.classList.add('is-invalid')
-    return false
-}
-;
+ 
+    var regEx=/^[A-Z][a-z ]{3,20}$/;
+   if ( regEx.test(productNameInput.value) ){
+    productNameInput.classList.replace('is-invalid', 'is-valid');
+    textValidateName.innerHTML='';
+    return true;
+   }
+   else{
+    productNameInput.classList.add('is-invalid');
+    textValidateName.innerHTML='Please enter your name start with capital letter';
 
+    return false;
+   }
+}
+
+function validateProductPrice(){
+    var regEx=/^[0-9]{1,}$/;
+   if ( regEx.test(productPriceInput.value)){
+    productPriceInput.classList.replace('is-invalid', 'is-valid');
+    textValidatePrice.innerHTML='';
+    return true;
+   }
+   else{
+    productPriceInput.classList.add('is-invalid');
+    textValidatePrice.innerHTML='The price shoud be only numbers';
+
+    return false;
+   }
+}
+function validateProductCategory(){
+    var regEx=/^[a-z A-Z]{5,30}$/;
+   if ( regEx.test(productCategoryInput.value)  ){
+    productCategoryInput.classList.replace('is-invalid', 'is-valid');
+    textValidateCategory.innerHTML='';
+    return true;
+   }
+   else{
+    productCategoryInput.classList.add('is-invalid');
+    textValidateCategory.innerHTML='The text size must be no less than 5 characters and no more than 30 characters.';
+
+    return false;
+   }
+}
+function validateProductDescription(){
+    var regEx=/^[a-z A-Z0-9]{5,90}$/;
+   if ( regEx.test(productDescriptionInput.value) ){
+    productDescriptionInput.classList.replace('is-invalid', 'is-valid');
+    textValidateDesc.innerHTML='';
+    return true;
+   }
+   else{
+    productDescriptionInput.classList.add('is-invalid');
+    textValidateDesc.innerHTML='The text size must be no less than 5 characters and no more than 30 characters.';
+    return false;
+   }
 
 }
